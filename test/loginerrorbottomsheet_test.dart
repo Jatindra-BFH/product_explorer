bdd608c039887ef0ff5963d111a1c73cf1f48755
Jatindra-BFH <jatindranath.mishra@breadfinancial.com>
2025-08-29 08:55:45 +0530

    updated a testCase

--- test/loginerrorbottomsheet_test.dart
+++ test/loginerrorbottomsheet_test.dart
@@ -3,10 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:product_explorer/presentation/widgets/login_error_bottom_sheet.dart';
 
 void main() {
-  testWidgets('ErrorBottomSheet displays message and closes on button tap', (WidgetTester tester) async {
+  testWidgets('LoginErrorBottomSheet displays message and closes on button tap', (WidgetTester tester) async {
     const testMessage = 'This is a test error message';
-
-    // Build a basic app with a button to open the bottom sheet
     await tester.pumpWidget(
       MaterialApp(
         home: Builder(
@@ -31,21 +29,16 @@ void main() {
         ),
       ),
     );
-
-    // Tap the button to open the bottom sheet
     await tester.tap(find.text('Open Error'));
     await tester.pumpAndSettle();
 
-    // Verify the error icon, message, and close button are present
     expect(find.byIcon(Icons.error), findsOneWidget);
     expect(find.text(testMessage), findsOneWidget);
     expect(find.text('Close'), findsOneWidget);
 
-    // Tap the "Close" button
     await tester.tap(find.text('Close'));
     await tester.pumpAndSettle();
 
-    // Bottom sheet should now be closed
     expect(find.byType(LoginErrorBottomSheet), findsNothing);
   });
 }
